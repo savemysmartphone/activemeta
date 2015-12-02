cf2a6bcc732289badad1a886be84af1627d9ae1d
Mehdi FARSI <mehdifarsi.pro@gmail.com>
2015-12-02 22:44:56 +0100

    fix(activemeta/context): remove unused variable

--- lib/active_meta/context.rb
+++ lib/active_meta/context.rb
@@ -3,7 +3,7 @@ module ActiveMeta
     class << self
       def new(&block)
         raise ArgumentError, 'no block given for context' unless block_given?
-        mod = Module.new do
+        Module.new do
           @eval_block = block
           class << self
             def valid_for_rule?(rule)
